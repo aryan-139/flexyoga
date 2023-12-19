c169359f352a71997a37a3da117acaa66bcbed9e
Aryan Raj <aryanraj2k25@gmail.com>
2023-12-20 04:17:46 +0530

    final edit

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const axiosApi = axios.create({
-    baseURL: 'http://localhost:80/',
-    // baseURL: 'https://uninterested-puce-undershirt.cyclic.app/',
+    // baseURL: 'http://localhost:80/',
+    baseURL: 'https://uninterested-puce-undershirt.cyclic.app/',
 })
 
 export default axiosApi;
