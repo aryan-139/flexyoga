8a9e18d79824a2f1404923209605dd120912e443
Aryan Raj <aryanraj2k25@gmail.com>
2023-12-20 01:26:31 +0530

    final commit 9

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosApi = axios.create({
-    baseURL: 'http://localhost:80',
+    baseURL: 'https://tan-unusual-blackbuck.cyclic.app',
 })
 
 export default axiosApi;
